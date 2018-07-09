171a2a62ba2af02e02af3938063693b3c771bbb2
linuxforse <linuxforse@gmail.com>
2018-07-09 22:03:48 +0300

    Добавлено всплывающее при окончании загрузки видео

--- youtube-bash.sh
+++ youtube-bash.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
+send0r="Фильм"
+send1r="Скачался"
+
 youload() {
-echo "Если хочешь скачать ещё видео - вставь ссылку, если нет - нажми клавиши CTRL + C"
+echo "Если хочешь скачать ещё видео - вставь ссылку, если нет - нажми клавиши C$
 unset var0
 read var0
-youtube-dl -Acitf 18 ${var0}
+youtube-dl -Acitf 18 ${var0} && notify-send -i vlc ${send0r} ${send1r} 
 youload
-	}
+        }
 
 cd ~/Загрузки
-echo "Привет ! Программа поможет тебе скачать видео из сети. Вставь ссылку на видео после этого текста:"
+echo "Привет ! Программа поможет тебе скачать видео из сети. Вставь ссылку на в$
 read var0
-youtube-dl -Acitf 18 ${var0}
+youtube-dl -Acitf 18 ${var0} && notify-send -i vlc ${send0r} ${send1r} 
 youload
 
 exit 0
